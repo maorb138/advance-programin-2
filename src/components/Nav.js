99e11360802d8f6df001324b821d7c033e0b37d2
omri rata <omri.rata@live.biu.ac.il>
2022-04-12 14:11:51 +0300

    added a navigation bar

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import './Nav.css';
+
+function Nav() {
+    
+    return (
+        <ul className="nav justify-content-end">
+            <a href="/" className="chat"><span className="header"><i className="bi bi-chat-left-text-fill"></i> ChatApp</span></a>
+            <li className="nav-item">
+                <a className="nav-link" href="/register" >Register</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/login" >Login</a>
+            </li>
+        </ul>
+		);
+}
+export default Nav;
