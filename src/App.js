81f62d0210fa2003b05b3a99c9e606d388d949f4
maorb138 <48924571+maorb138@users.noreply.github.com>
2022-04-12 13:48:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Nav from './Nav';
-import Main from './Main';
+import Nav from './components/Nav';
+import Main from './components/Main';
 import { useState } from 'react';
 import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
 /*
