06a017d4634be8c358cf75b75740fb32e1aa19f7
maor <maor138@gmail.com>
2022-04-16 20:55:52 +0300

    test

--- src/components/ChatForm.js
+++ src/components/ChatForm.js
@@ -1,12 +1,118 @@
 import './ChatForm.css'
 function ChatForm(){
     return(
+        <div id="container">
+  <aside>
+    <header>
+        <h3>Welcome back!</h3>
+      <input type="text" placeholder="search" />
+    </header>
+    <ul>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_01.jpg" alt />
         <div>
-         
-        <h1> mmaoe</h1>
-        
-        
+          <h2>Prénom Nom</h2>
+          <h3>
+            <span className="status orange" />
+            offline
+          </h3>
         </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_02.jpg" alt />
+        <div>
+          <h2>Prénom Nom</h2>
+          <h3>
+            <span className="status green" />
+            online
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_03.jpg" alt />
+        <div>
+          <h2>Prénom Nom</h2>
+          <h3>
+            <span className="status orange" />
+            offline
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_04.jpg" alt />
+        <div>
+          <h2>Prénom Nom</h2>
+          <h3>
+            <span className="status green" />
+            online
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_05.jpg" alt />
+        <div>
+          <h2>Prénom Nom</h2>
+          <h3>
+            <span className="status orange" />
+            offline
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_06.jpg" alt />
+        <div>
+          <h2>Prénom Nom</h2>
+          <h3>
+            <span className="status green" />
+            online
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_07.jpg" alt />
+        <div>
+          <h2>Prénom Nom</h2>
+          <h3>
+            <span className="status green" />
+            online
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_08.jpg" alt />
+        <div>
+          <h2>Prénom Nom</h2>
+          <h3>
+            <span className="status green" />
+            online
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_09.jpg" alt />
+        <div>
+          <h2>Prénom Nom</h2>
+          <h3>
+            <span className="status green" />
+            online
+          </h3>
+        </div>
+      </li>
+      <li>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_10.jpg" alt />
+        <div>
+          <h2>Prénom Nom</h2>
+          <h3>
+            <span className="status orange" />
+            offline
+          </h3>
+        </div>
+      </li>
+    </ul>
+  </aside>
+
+</div>
+
     );
 }
 
