db1efe1eb2146518698a69a14dea80a146b29e90
maor <maor138@gmail.com>
2022-04-16 20:57:49 +0300

    q

--- src/components/ChatForm.js
+++ src/components/ChatForm.js
@@ -110,7 +110,90 @@ function ChatForm(){
       </li>
     </ul>
   </aside>
-
+  <main>
+    <header>
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_01.jpg" alt />
+      <div>
+        <h2>Chat with Vincent Porter</h2>
+        <h3>already 1902 messages</h3>
+      </div>
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_star.png" alt />
+    </header>
+    <ul id="chat">
+      <li className="you">
+        <div className="entete">
+          <span className="status green" />
+          <h2>Vincent</h2>
+          <h3>10:12AM, Today</h3>
+        </div>
+        <div className="triangle" />
+        <div className="message">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+        </div>
+      </li>
+      <li className="me">
+        <div className="entete">
+          <h3>10:12AM, Today</h3>
+          <h2>Vincent</h2>
+          <span className="status blue" />
+        </div>
+        <div className="triangle" />
+        <div className="message">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+        </div>
+      </li>
+      <li className="me">
+        <div className="entete">
+          <h3>10:12AM, Today</h3>
+          <h2>Vincent</h2>
+          <span className="status blue" />
+        </div>
+        <div className="triangle" />
+        <div className="message">
+          OK
+        </div>
+      </li>
+      <li className="you">
+        <div className="entete">
+          <span className="status green" />
+          <h2>Vincent</h2>
+          <h3>10:12AM, Today</h3>
+        </div>
+        <div className="triangle" />
+        <div className="message">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+        </div>
+      </li>
+      <li className="me">
+        <div className="entete">
+          <h3>10:12AM, Today</h3>
+          <h2>Vincent</h2>
+          <span className="status blue" />
+        </div>
+        <div className="triangle" />
+        <div className="message">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+        </div>
+      </li>
+      <li className="me">
+        <div className="entete">
+          <h3>10:12AM, Today</h3>
+          <h2>Vincent</h2>
+          <span className="status blue" />
+        </div>
+        <div className="triangle" />
+        <div className="message">
+          OK
+        </div>
+      </li>
+    </ul>
+    <footer>
+      <textarea placeholder="Type your message" defaultValue={""} />
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png" alt />
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png" alt />
+      <a href="#">Send</a>
+    </footer>
+  </main>
 </div>
 
     );
