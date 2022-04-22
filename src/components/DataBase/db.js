9c682a18092207b78f6e2951c49e95ff4d051fc3
maorb138 <maorb138@gmail.com>
2022-04-22 18:14:46 +0300

    m\

--- src/components/DataBase/db.js
+++ src/components/DataBase/db.js
@@ -1,10 +1,10 @@
 
 
 const users = [{ username: 'avi',nickname:'avi1', password: '1234' ,image:''},
-    { username: 'avi1',nickname:'avi1', password: '0000', image: ''},
-    { username: 'avi2',nickname:'avi2', password: '1212', image: ''},
-    { username: 'a',nickname:'a', password: 'Ma1234567', image: ''},
-    { username: 'maor',nickname:'biton', password: 'Ma1234567', image: ''}]
-    
-    export default users;
-     
+{ username: 'avi1',nickname:'avi1', password: '0000', image: ''},
+{ username: 'avi2',nickname:'avi2', password: '1212', image: ''},
+{ username: 'a',nickname:'a', password: 'Ma1234567', image: ''},
+{ username: 'maor',nickname:'biton', password: 'Ma1234567', image: ''}]
+
+export default users;
+ 
