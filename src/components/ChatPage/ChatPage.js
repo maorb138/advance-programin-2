0ec05786affa50bb57801a69cbc71e0ae07623f3
omri rata <omri.rata@live.biu.ac.il>
2022-04-22 20:56:41 +0300

    design the chat page

--- src/components/ChatPage/ChatPage.js
+++ src/components/ChatPage/ChatPage.js
@@ -1,23 +1,82 @@
 
+import { useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import './ChatPage.css';
+import Contacts from './Contacts';
+import Messages from './Messages';
 
-function ChatPage() {
+function ChatPage({ users, addContacts }) {
+    var messages1 = ['c1', 'c2', 'c1', 'c2', 'c1', 'c2', 'c1', 'c2', 'c1', 'c2', 'c1', 'c2', 'c2'];
+    const [messages, setMess] = useState(messages1);
+    const [user, setUser] = useState(useLocation().state);
+    const [contacts, setcontacts] = useState(user.contacts);
+    //document.getElementsByClassName('mainChat')[0].scrollTop = document.getElementsByClassName('mainChat')[0].scrollHeight;
+
+
+    const showMessege = (e) => {
+        var message = document.getElementsByClassName("textinp")[0];
+        if (e.keyCode === 13 ){
+            console.log('--------->', messages, e.target);
+            const newMess = messages.concat([e.target.value]);
+            setMess(newMess);
+            console.log('--------->', messages);
+            message.value = '';
+        }
+        else if (e.target.tagName.toLowerCase() == 'button') {
+            console.log(message.value);
+            const newMess = messages.concat(message.value);
+            setMess(newMess);
+            message.value = '';
+
+        }
+    }
+
+    const addFriend = function (username) {
+
+        let flag = 1, count = 0;
+
+        if (username == user.username) {
+            console.log("cant add yourself");
+            return;
+        }
+        flag = user.contacts.forEach((i) => {
+            if (username == i.username) {
+                console.log("already friends");
+                return 0;
+            }
+            return 1;
+        })
+        console.log(contacts);
+        users.map((x) => {
+            if (x.username !== username)count++;
+
+        });
+        if (count == users.lenght) {
+            console.log('not exists');
+            return;
+        }
+        console.log('now you and', username, 'are friends');
+        console.log(contacts)
+        //setcontacts(contacts.concat([{ username: 'user1', mem: '' }]))
+        addContacts(user, [{ username: 'user1', mem: '' }]);
+
+    }
+    //console.log(location.state);
     return (
         <div className="chat1">
-            <nav>
-                <div className="hello nav nav-tabs" id="nav-tab" role="tablist">
-                    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
-                    <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
-                    <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button>
-                </div>
-            </nav>
-            <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">Home</div>
-                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">Profile</div>
-                <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">Contact</div>
+            {user !== null ? < Contacts user={user} contacts={contacts} users={users} addFriend={addFriend} /> : null}
+
+            <div className="mainChat">
+                <Messages messages={messages} />
             </div>
-            hello world;
-        </div>
+            <div className="addButtons">
+                hello world
+                </div>
+            <div className="form-control1">
+                <input autoComplete="off" onKeyDown={showMessege } className="textinp" type="text" id="formGroupExampleInput" placeholder="Enter a messege"></input>
+                <button onClick={showMessege } type="button" class="btn btn-success">Send</button>
+            </div>
+         </div>
         );
 }
 
