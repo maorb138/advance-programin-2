f365c4f903557e16c01ad401a27663afd1b9d775
omri rata <omri.rata@live.biu.ac.il>
2022-04-23 13:53:38 +0300

    added contacts to the users

---
+++ src/components/DataBase/db.js
@@ -0,0 +1,9 @@
+
+
+const users = [{ username: 'user1', nickname: 'user1', password: '1234', image: '', contacts: [{ username: 'user3', mem: '' }, { username: 'user2', mem: '' }]},
+    { username: 'user2', nickname: 'user2', password: '0000', image: '', contacts: [{ username: 'user1', mem: '' }, { username: 'user4', mem: '' }]},
+{ username: 'user3', nickname: 'user3', password: '1212', image: '', contacts: [{ username: 'user4', mem: '' }, { username: 'user2', mem: '' }]},
+{ username: 'user4', nickname: 'user4', password: 'Ma1234567', image: '', contacts: [{ username: 'user1', mem: '' }, { username: 'user3', mem: '' }] }]
+
+export default users;
+ 
