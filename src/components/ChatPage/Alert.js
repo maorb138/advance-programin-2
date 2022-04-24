4efd2316ad8f50d8c38c007827078a54843d5123
omri rata <omri.rata@live.biu.ac.il>
2022-04-24 17:41:46 +0300

    add alert

---
+++ src/components/ChatPage/Alert.js
@@ -0,0 +1,19 @@
+import NotConnect from './../Image/uncon.png';
+import './Sidebar.css';
+function Alert(){
+
+
+return(
+<div >
+<a href='./soon'>
+<img id="alert" className="alert" src={NotConnect}/>
+</a>
+ </div>
+
+);
+
+
+}
+
+
+export default Alert;
