b22aa1dbd8e04b2203861b542cd4fdc4c1fb1d44
maorb138 <maorb138@gmail.com>
2022-04-24 15:27:43 +0300

    push

--- src/components/ChatPage/Contacts.js
+++ src/components/ChatPage/Contacts.js
@@ -1,7 +1,9 @@
 import './ChatPage.css';
 import { NavLink, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-
+import Avatar from './../Image/profile-picture-boy-2.jpeg';
+import './Sidebar.css';
+import './PopupImg.css'
 
 
 function Contacts({ user, contacts, users, addFriend, addContact }) {
@@ -18,9 +20,15 @@ function Contacts({ user, contacts, users, addFriend, addContact }) {
     return (
         <div class="card">
             <div class="card-header">
-                <h3>
+                <h6>
                     hello {user.username}
-                    </h3>
+                    </h6>
+                    <img id="myImg" className='myImg' onClick={PopupImg} src={Avatar} />
+                    <div id="myModal" class="modal">
+                    <span class="close">&times;</span>
+                    <img class="modal-content" id="img01"/>
+                    <div id="caption"></div>
+                  </div>
                 <input className="searchBox" type='search' placeholder='search'></input>
                 </div>
                 <ul class="list-group list-group-flush">
@@ -30,6 +38,28 @@ function Contacts({ user, contacts, users, addFriend, addContact }) {
             <button type="button" onClick={() => { addFriend( 'user3') }} className="btn btn-primary addFriend">Add a Friend</button>
             </div>
         )
+}
+function PopupImg(){
+    var modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById("myImg");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    img.onclick = function(){
+      modal.style.display = "block";
+      modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
+    }
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+      modal.style.display = "none";
+    }
+
 }
 
 export default Contacts;
