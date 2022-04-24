a5254f2d22c3fb900927c310c840b9a7ac7dae39
maorb138 <maorb138@gmail.com>
2022-04-24 16:40:59 +0300

    popupimg

--- src/components/ChatPage/Contacts.js
+++ src/components/ChatPage/Contacts.js
@@ -3,7 +3,8 @@ import { NavLink, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Avatar from './../Image/profile-picture-boy-2.jpeg';
 import './Sidebar.css';
-import './PopupImg.css'
+import './PopupImg.css';
+import { PopupImg } from './Sidebar.js';
 
 
 function Contacts({ user, contacts, users, addFriend, addContact }) {
@@ -39,27 +40,6 @@ function Contacts({ user, contacts, users, addFriend, addContact }) {
             </div>
         )
 }
-function PopupImg(){
-    var modal = document.getElementById("myModal");
 
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var img = document.getElementById("myImg");
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    img.onclick = function(){
-      modal.style.display = "block";
-      modalImg.src = this.src;
-      captionText.innerHTML = this.alt;
-    }
-    
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-    
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() { 
-      modal.style.display = "none";
-    }
-
-}
 
 export default Contacts;
