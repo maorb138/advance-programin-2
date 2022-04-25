d82c9e75e6c575100b670444353444e757eb81a1
omri rata <omri.rata@live.biu.ac.il>
2022-04-25 15:32:24 +0300

    change the validation for the username and the password

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,14 +4,12 @@ import React from 'react';
 import './../DataBase/db.js';
 import { useHistory } from "react-router-dom";
 import  users from './../DataBase/db.js';
-import UserData from '../DataBase/UserData';
 import Sidebar from './../ChatPage/Sidebar';
 
 
 function Register(props) {
     const history = useHistory();
 
-    const [err, seterr] = useState("");
     const [file, setfile] = useState(null);
     
     const registerValid = () => {
@@ -19,77 +17,31 @@ function Register(props) {
         var nickname = document.getElementById("nickname").value;
         var password = document.getElementById("password").value;
         var confirmPass = document.getElementById("confirmPass").value;
-        var myImage =  document.getElementById("image-input").files.length 
-        UserData.userN = username;
         console.log(username, nickname, password, confirmPass);
-        passwordValidation(password, confirmPass);
-         NickNameValidation(nickname);
-         ImageValidation(myImage);
-    
 
     
-        /*
-        props.users.push({ username: "omri", password: "1010" });
-        var new_users = props.users;    
-        console.log(new_users,11);
-        props.func(new_users);
-        var navLink = document.getElementsByClassName('nav-link');
-        navLink[0].style.visibility = "hidden";
-        navLink[1].style.visibility = "hidden";
-        */ 
-        console.log(username, nickname, password, confirmPass);
-        if (passwordValidation(password, confirmPass)) {
-            console.log("its ok");
-            UserData.passW=password;
-            
-        } else {
-            console.log("its NOT ok");
-        }
 
-        function NickNameValidation(nickname){
-            if(nickname===''){
-              ////no nickname so i create a new one called NewnickName;
-              var threeLeeter= username.substring(0,2);
-              var Newnickname=threeLeeter+Math.floor(Math.random() * 50)+Math.random().toString(36).replace(/[^a-z]+/g, '').substring(0, 2);
-              
-            alert("you didnt chose nickname");
-            alert("your nickname is:"+Newnickname);
-            nickname=Newnickname;
-            UserData.nickN=Newnickname;
-              return nickname;
-            }else{
-                 ///he wrote nickname in field;
-                console.log(nickname)
-                UserData.nickN=nickname;
-                return nickname;  
-            }
-        }
-        var count = 0;
-        console.log("*************************************")
-        props.addUsers(props.users.concat([{ username: "user1", password: "1010", image: file, contacts: [{ username: '', mem: '' }, { username: '', mem: '' }] }]));
+        
+        var flag = true;
 
         users.map((x) => {
-        
 
-            if (username === x.username && username == x.username) {
-                alert("An existing username in the system");
-                count++;
-                    
-            }  if(username !== x.username && username!=x.username ){
-              
-            } 
-            if(count>=1){
-                users.pop(UserData);
-        history.push("/");  
-            }else{
-               
-        //history.push("/chat");
-
-
-            }           
+            if (username === x.username) {
+                console.log("An existing username in the system");
+                flag = false;
+            }          
         });
 
 
+        if (passwordValidation(password, confirmPass) && username !=='' && flag) {
+            console.log("its ok");
+            const newUser = { username: username, password: password, image: file, contacts: [{ username: 'hi', mem: [{ sent: false, message: '' }, { sent: false, message: '' }] }, { username: 'bye', mem: [{ sent: true, message: '' }, { sent: true, message: '' }] }] };
+            props.addUsers(props.users.concat([newUser ]));
+            history.push('/chat', newUser);
+
+        } else {
+            console.log("its NOT ok");
+        }
 
     }
     return (
@@ -113,12 +65,11 @@ function Register(props) {
                 <input id="confirmPass" type="password" className="form-control inp" required></input>
             </div>
             <div>
-                <input type="file" id="image-input" onChange={(e) => setfile(URL.createObjectURL(e.target.files[0]))} conClick={displayimage} accept="image/jpeg, image/png, image/jpg" />
+                <input type="file" id="image-input" onChange={(e) => setfile(URL.createObjectURL(e.target.files[0]))} onClick={displayimage} accept="image/jpeg, image/png, image/jpg" />
             <div id="display-image" />
             </div>
-            <img src={file}></img>
             <div className="col-12">
-                <button type="submit" onClick={registerValid} className="btn btn-primary mb-1">Sign-up</button>
+                <button type="button" onClick={registerValid} className="btn btn-primary mb-1">Sign-up</button>
                
             </div>
         </form>
@@ -168,15 +119,15 @@ function passwordValidation(pass, confPass) {
 function displayimage(){
     const image_input = document.querySelector("#image-input");
 
-image_input.addEventListener("change", function() {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => {
-    const uploaded_image = reader.result;
-    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
-  });
-  reader.readAsDataURL(this.files[0]);
+    image_input.addEventListener("change", function() {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        const uploaded_image = reader.result;
+        document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+      });
+      reader.readAsDataURL(this.files[0]);
     });
-    }
+}
     function ImageValidation(myImage){
         if(myImage===0){    
             alert("you didnt choose any image");
@@ -184,10 +135,8 @@ image_input.addEventListener("change", function() {
             let i = Math.floor(Math.random() * 25);
            var newImage=window.open('./Emoji/'+i+'.png', "", "width=400,height=400");
            myImage=newImage;
-           UserData.imgU='abc';
            return myImage;
         }else{
-            UserData.imgU='abc';
             return myImage;
         }
     }
